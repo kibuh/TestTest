f25993d1c898c43dc5d6b060fa075562070e18d6
kibuh <gkibuhguy@gmail.com>
2022-03-26 12:27:27 +0100

    -added enpoints to add and remove magazines

--- src/main/java/com/kibuh/tstestexo2/controllers/MagazineController.java
+++ src/main/java/com/kibuh/tstestexo2/controllers/MagazineController.java
@@ -1,5 +1,6 @@
 package com.kibuh.tstestexo2.controllers;
 
+import com.kibuh.tstestexo2.entities.Produit;
 import com.kibuh.tstestexo2.payload.request.MagazineRequestModel;
 import com.kibuh.tstestexo2.payload.request.ProductRequestModel;
 import com.kibuh.tstestexo2.payload.response.MagazineResponseModel;
@@ -7,6 +8,7 @@ import com.kibuh.tstestexo2.payload.response.ProductResponseModel;
 import com.kibuh.tstestexo2.services.MagazineService;
 import com.kibuh.tstestexo2.services.ProduitService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -27,5 +29,17 @@ public class MagazineController {
     @GetMapping
     public List<MagazineResponseModel> getMagazines(@RequestParam(value = "page",defaultValue = "0") int page, @RequestParam(value = "limit", defaultValue = "5") int limit){
         return magazineService.getAllMagazine(page, limit);
+    }
+
+    @PostMapping("/{ magazine_id}/add-produit")
+    public ProductResponseModel addProduit(@PathVariable(" magazine_id") long magazine_id, @RequestBody Produit produit){
+
+        return magazineService.addproduct(magazine_id,produit);
+    }
+
+    @PostMapping("/{ magazine_id}/remove-produit")
+    public ResponseEntity removeProduit(@PathVariable(" magazine_id") long magazine_id, @RequestBody Produit produit){
+
+        return magazineService.removeProduct(magazine_id,produit);
     }
 }
--- src/main/java/com/kibuh/tstestexo2/services/MagazineService.java
+++ src/main/java/com/kibuh/tstestexo2/services/MagazineService.java
@@ -1,24 +1,30 @@
 package com.kibuh.tstestexo2.services;
 
 import com.kibuh.tstestexo2.entities.Magazine;
+import com.kibuh.tstestexo2.entities.Produit;
 import com.kibuh.tstestexo2.payload.request.MagazineRequestModel;
 import com.kibuh.tstestexo2.payload.response.MagazineResponseModel;
+import com.kibuh.tstestexo2.payload.response.ProductResponseModel;
 import com.kibuh.tstestexo2.rrepositories.MagazinRepository;
+import com.kibuh.tstestexo2.rrepositories.ProductRepository;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 @Service
 public class MagazineService {
 
     @Autowired
     MagazinRepository magazinRepository;
+
+    @Autowired
+    ProductRepository productRepository;
 
     @Autowired
     ModelMapper modelMapper;
@@ -41,6 +47,54 @@ public class MagazineService {
         magazines.forEach(magazine -> magazineResponseModels.add(modelMapper.map(magazine, MagazineResponseModel.class)));
 
         return magazineResponseModels;
+
+    }
+
+    public ProductResponseModel addproduct(long magazine_id, Produit produit){
+
+        return magazinRepository.findById(magazine_id).map(magazine ->{
+
+            Optional<Produit> exitProduct = productRepository.findById(produit.getId());
+
+            if(exitProduct.isPresent()){
+
+                magazine.getProduits().add(exitProduct.get());
+
+                magazinRepository.save(magazine);
+
+                return modelMapper.map(exitProduct,ProductResponseModel.class);
+            }
+
+            Produit produit1 = productRepository.save(produit);
+
+            magazine.getProduits().add(produit1);
+
+            return modelMapper.map(produit1,ProductResponseModel.class);
+
+        }).orElseThrow(() -> new RuntimeException("Magazine with id " +magazine_id +"  does not exist"));
+
+    }
+
+    public ResponseEntity  removeProduct(long magazine_id, Produit produit){
+
+       return magazinRepository.findById(magazine_id).map(magazine -> {
+           ResponseEntity<Map<String, String>> mapResponseEntity = productRepository.findById(produit.getId()).map(produit1 -> {
+
+               magazine.getProduits().remove(produit1);
+
+               magazinRepository.save(magazine);
+
+               Map<String, String> response = new HashMap<>();
+
+               response.put("status", "Produit removed");
+
+               return ResponseEntity.ok(response);
+
+
+           }).orElseThrow(() -> new RuntimeException("Produit with id " + produit.getId() + "  does not exist"));
+           return mapResponseEntity;
+
+       }).orElseThrow(() -> new RuntimeException("Magazine with id " +magazine_id +"  does not exist"));
 
     }
 }

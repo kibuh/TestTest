4c86866074d6abafb17ce2e1382ec0523b76a8e5
kibuh <gkibuhguy@gmail.com>
2022-03-26 12:03:53 +0100

    -added manytomany mapping betwwen produit and magazine

--- src/main/java/com/kibuh/tstestexo2/entities/Magazine.java
+++ src/main/java/com/kibuh/tstestexo2/entities/Magazine.java
@@ -7,6 +7,8 @@ import lombok.Setter;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Getter
@@ -21,4 +23,12 @@ public class Magazine implements Serializable {
 
     @Column(nullable = false)
     private String nom;
+
+
+
+    @ManyToMany(fetch = FetchType.LAZY, cascade = {CascadeType.PERSIST, CascadeType.MERGE})
+    @JoinTable(name = "magazine",
+            joinColumns = {@JoinColumn(name = "magazine_id")},
+            inverseJoinColumns = {@JoinColumn(name="produit_id")})
+    private Set<Produit> produits= new HashSet<>();
 }
--- src/main/java/com/kibuh/tstestexo2/entities/Produit.java
+++ src/main/java/com/kibuh/tstestexo2/entities/Produit.java
@@ -1,15 +1,14 @@
 package com.kibuh.tstestexo2.entities;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.annotations.OnDelete;
-import org.hibernate.annotations.OnDeleteAction;
 
 import javax.persistence.*;
 import java.io.Serializable;
+
+import java.util.Set;
 
 @Entity
 @Getter
@@ -28,10 +27,10 @@ public class Produit implements Serializable {
     @Column(nullable = false)
     private Double prix;
 
-//    @ManyToOne(fetch = FetchType.EAGER,optional = false, cascade = {CascadeType.ALL})
-//    @JoinColumn(name = "magazine_id")
-//    @OnDelete(action = OnDeleteAction.CASCADE)
-//    @JsonIgnore
-//    private Magazine magazine;
+
+    @ManyToMany(fetch = FetchType.LAZY,
+            cascade = { CascadeType.PERSIST,CascadeType.MERGE},mappedBy = "produits")
+    private Set<Magazine> magazines;
+
 
 }

dc48eafe2b4f55df73d7b8bd220b37f37fd3f376
kibuh <gkibuhguy@gmail.com>
2022-03-26 10:33:51 +0100

    initial commit

---
+++ src/test/java/com/kibuh/tstestexo2/TsTestExo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kibuh.tstestexo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TsTestExo2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
